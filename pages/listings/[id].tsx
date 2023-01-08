60f400f489499d334ad04ee4206679f2abc995af
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2023-01-08 19:47:14 +0200

    m

--- pages/listings/[id].tsx
+++ pages/listings/[id].tsx
@@ -14,6 +14,7 @@ import { propertyProp } from "types/listingInterface";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { apiURL } from "constants/apiURL";
+import { defaultListing } from "constants/defaultListing";
 
 export const getStaticPaths = async () => {
   const paths = await getAllListingsIds();
@@ -32,26 +33,6 @@ export const getStaticProps = async ({ params }: any) => {
       listingData: JSON.parse(JSON.stringify(listingData)),
     },
   };
-};
-
-const defaultListing = {
-  id: 2,
-  name: "Luxurious Beachfront Villa",
-  address: "456 Ocean Dr, Miami, FL 33139",
-  price: 300,
-  guests: 8,
-  bedrooms: 4,
-  bathrooms: 3,
-  photos: [
-    "https://source.unsplash.com/B0aCvAVSX8E",
-    "https://source.unsplash.com/vvah86ql8iM",
-    "https://source.unsplash.com/MP0bgaS_d1c",
-    "https://source.unsplash.com/fBdlytm6Hp8",
-    "https://source.unsplash.com/AQl-J19ocWE",
-  ],
-  amenities: ["wifi", "kitchen", "pool", "tv"],
-  description:
-    "This luxurious beachfront villa is located in Miami and can accommodate up to 8 guests. It features a full kitchen, pool, and TVs in every bedroom. The outdoor patio is perfect for BBQs and watching the sunset.",
 };
 
 export default function Property({ listingData = defaultListing }: any) {
